d54f9ec337a9de731135bbe1111d6124092ab249
Andrey Melnikov <a.melnikov1310@ya.ru>
2021-01-17 15:04:38 +0300

    ready

---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -0,0 +1,27 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyLoggingAspect {
+
+    @Around("execution(* com.zaurtregulov.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        MethodSignature signature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String nameMethod = signature.getName();
+
+        System.out.println("Begin of: " + nameMethod);
+
+        Object resultTargetMethod = proceedingJoinPoint.proceed();
+
+        System.out.println("Ends of: " + nameMethod);
+
+        return resultTargetMethod;
+
+    }
+}
---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -0,0 +1,49 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.dao;
+
+import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+    @Autowired
+    private SessionFactory factory;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        Session session = factory.getCurrentSession();
+//        List<Employee> employees = session.createQuery("from Employee", Employee.class).getResultList();
+        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+        List<Employee> employees = query.getResultList();
+
+        return employees;
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        Session session = factory.getCurrentSession();
+        session.saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = factory.getCurrentSession();
+
+        return session.get(Employee.class, id);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = factory.getCurrentSession();
+        Query<Employee> query = session.createQuery("delete from Employee where id=:empId");
+        query.setParameter("empId", id);
+        query.executeUpdate();
+    }
+}
